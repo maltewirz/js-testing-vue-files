f9d647c71666b8684585e0b17a6f7083d50781ae
Malte Wirz <mail@maltewirz.com>
2020-07-01 14:41:46 +0200

    testing vue methods

--- specs/temp.spec.js
+++ specs/temp.spec.js
@@ -1,2 +1,37 @@
-import Temprature from '@/temprature'
+import Temperature from '@/temperature'
 import { mount } from '@vue/test-utils'
+
+
+describe('computed', () => {
+    test('celsius', () => {
+        const { vm } = mount(Temperature)
+        expect(vm.celsius).toBe(0)
+        vm.degrees = 23
+        expect(vm.celsius).toBe(23)
+    })
+
+    test('fahrenheit', () => {
+        const { vm } = mount(Temperature)
+        expect(vm.fahrenheit).toBe(32)
+        vm.degrees = 16
+        expect(vm.fahrenheit).toBe(60.8)
+    })
+});
+
+test('temp watcher', async () => {
+    const wrapper = mount(Temperature, {
+        propsData: {
+            temp: 40
+        }
+    })
+    const { vm } = wrapper
+    expect(vm.degrees).toBe(40)
+    expect(vm.type).toBe('celsius')
+
+    await wrapper.setProps({
+        temp: '50f'
+    })
+
+    expect(vm.degrees).toBe(50)
+    expect(vm.type).toBe('fahrenheit')
+})
