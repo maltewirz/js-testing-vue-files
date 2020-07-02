89783897927dfc606ce07b331927d1712b0de46a
Malte Wirz <mail@maltewirz.com>
2020-07-02 09:39:38 +0200

    vuex store

--- specs/salad.spec.js
+++ specs/salad.spec.js
@@ -1,6 +1,31 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
 
 import SaladBowlComponent from '@/salad-bowl'
 import saladStore from '@/store/salad-store'
+
+// Local Vue avoids polluting the global Vue instance
+const VueWithVuex = createLocalVue();
+VueWithVuex.use(Vuex)
+
+let store
+let wrapper
+
+beforeEach(() => {
+    store = new Vuex.Store(saladStore)
+    wrapper = mount(SaladBowlComponent, {
+        localVue: VueWithVuex,
+        store
+    })
+})
+
+test('store is loaded', () => {
+    store.state.salad.push('cucumber')
+    expect(wrapper.vm.salad).toEqual(['cucumber'])
+})
+
+test('store works', () => {
+    wrapper.vm.addIngredient('tomato')
+    expect(wrapper.vm.salad).toEqual(['tomato'])
+})
