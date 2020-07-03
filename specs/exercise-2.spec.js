70363319246ce043410b58257d1e191380e3bd6f
Malte Wirz <mail@maltewirz.com>
2020-07-03 07:58:59 +0200

    exercise 2

--- specs/exercise-2.spec.js
+++ specs/exercise-2.spec.js
@@ -1,2 +1,25 @@
 import ExerciseForm from '@/exercise-2';
 import { mount } from '@vue/test-utils';
+import { nextTick } from 'vue';
+
+test('follow the user through the form', async () => {
+    const wrapper = mount(ExerciseForm)
+
+    expect(wrapper).toMatchSnapshot()
+    
+    const form = wrapper.find('form')
+
+    const input = form.find('input')
+    const taskList = wrapper.find('ul')
+
+    input.setValue('my todo')
+    await form.trigger('submit')
+
+    expect(wrapper).toMatchSnapshot()
+
+    const button = taskList.find('li').find('button')
+    
+    await button.trigger('click')
+
+    expect(wrapper).toMatchSnapshot()
+})
