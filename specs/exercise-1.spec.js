af99cd4481693ae47362a04969eeb4181ed7bf37
Malte Wirz <mail@maltewirz.com>
2020-07-03 07:38:02 +0200

    exercise 1

--- specs/exercise-1.spec.js
+++ specs/exercise-1.spec.js
@@ -1,2 +1,25 @@
 import UserList from '@/exercise-1';
 import { mount } from '@vue/test-utils';
+import { name } from 'faker';
+
+test('component renders the users', async () => {
+    const wrapper = mount(UserList, {
+        propsData: {
+            users: [name.findName(), name.findName(),
+                name.findName()]
+        }
+    })
+    // console.log(wrapper.props('users'));
+    
+    const li = wrapper.findAll('li')
+    const person = li.at(1).text();
+    
+    expect(li.length).toBe(3)
+    
+    await wrapper.find('input').setValue(person)
+
+    const finalLi = wrapper.find('li')
+    expect(finalLi.text()).toBe(person)
+
+    
+})
